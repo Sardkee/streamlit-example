5f61470587d637ff85ea91f1d0006315577c1634
Sardkee <38195831+Sardkee@users.noreply.github.com>
2023-03-17 14:42:28 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,44 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+import io
+import PyPDF2
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+def merge_pdfs(pdfs):
+    merger = PyPDF2.PdfMerger()
+    for pdf in pdfs:
+        merger.append(pdf)
+    merged_pdf = io.BytesIO()
+    merger.write(merged_pdf)
+    return merged_pdf
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+def extract_pages(pdf, pages):
+    reader = PyPDF2.PdfReader(pdf)
+    writer = PyPDF2.PdfWriter()
+    page_ranges = (x.split("-") for x in pages.split(","))
+    range_list = [i for r in page_ranges for i in range(int(r[0]), int(r[-1]) + 1)]
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+    for p in range_list:
+        # Subtract 1 to deal with 0 index
+        writer.add_page(reader.pages[p - 1])
+    extracted_pdf = io.BytesIO()
+    writer.write(extracted_pdf)
+    return extracted_pdf
 
-    points_per_turn = total_points / num_turns
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+st.title("PDF Manipulation")
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+uploaded_files = st.file_uploader("Upload PDF files", type="pdf", accept_multiple_files=True)
+
+if uploaded_files:
+    if len(uploaded_files) > 1:
+        if st.button("Merge PDFs"):
+            merged_pdf = merge_pdfs(uploaded_files)
+            st.download_button(label="Download Merged PDF", data=merged_pdf.getvalue(), file_name="merged.pdf", mime="application/pdf")
+    else:
+        pdf = uploaded_files[0]
+        st.write(f"Total Pages: {len(PyPDF2.PdfReader(pdf).pages)}")
+        pages = st.text_input("Please enter pages to extract (e.g. 1-3, 5, 7)")
+        if st.button("Extract Pages"):
+            extracted_pdf = extract_pages(pdf, pages)
+            st.download_button(label="Download Extracted Pages", data=extracted_pdf.getvalue(), file_name="extracted.pdf", mime="application/pdf")
